f92f9e6cbac15f00cc07c5c56c4258a9c2b76f2e
Joris-rtn <routin.joris@gmail.com>
2020-04-12 17:03:51 +0200

    Ajout armure, barbe, yeux

---
+++ Assets/Scripts/Character/clothes/CopyBones.cs
@@ -0,0 +1,33 @@
+﻿//Script for video tutorial https://www.youtube.com/watch?v=sc6kq1TV8Ek by TheoremGames
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CopyBones : MonoBehaviour
+{
+
+    public GameObject SourceObject;
+
+    // Use this for initialization
+    void Start()
+    {
+        Copy();
+    }
+
+    void Copy()
+    {
+        if (SourceObject == null) return;
+
+        var sourceRenderer = SourceObject.GetComponent<SkinnedMeshRenderer>();
+        var targetRenderer = GetComponent<SkinnedMeshRenderer>();
+
+        if (sourceRenderer == null) return;
+        if (targetRenderer == null) return;
+
+        targetRenderer.bones =
+            sourceRenderer.bones.Where(b => targetRenderer.bones.Any(t => t.name == b.name)).ToArray();
+
+
+    }
+}
